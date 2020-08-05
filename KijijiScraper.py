0a070823c392d94ed09eefa0f7e3c500c6a69ab6
Nicholas <nicholas.guthrie4@gmail.com>
2020-08-05 11:32:33 -0400

    Refactored for single function to handle ad parsing.

--- KijijiScraper.py
+++ KijijiScraper.py
@@ -14,64 +14,98 @@ def get_soup(url):
 
 soup = get_soup('https://www.kijiji.ca/b-ontario/art/k0l9004?dc=true')
 
-# refactor this code:
-# get clearfix class, which has all info, get images and price, save together
-# instead of in two separate loops
-# will need to go into the urls to get the full size pictures...
+# TO DO: filter for good ads
+# 1) remove wanted in title
+# 2) remove non-numeric ads
+# 3)
 
 
-# NOTE: not working
-ads = soup.find_all('div', {'class': 'clearfix'})
-for ad in ads:
-    # using regrex to strip internal empty space
-    #print(re.sub(' +', ' ', ad.find('a', {"class": "title"}).text.strip()))
-    print(ad.find('a', {"class": "title"}).text.strip())
-    print(ad.find('div', {"class": "price"}).text.strip())
-    print(ad.find('image'))
+class Picture:   # no parenthesis if not sub-classing, i.e. class_name(is subclass of this class)
+
+    def __init__(self, title, price, img_link):
+        self.title = title
+        self.price = price
+        self.img_link = img_link
 
 
-# this is the syntax for getting attributes with specific tags
-info_containers = soup.find_all('div', {"class": "info-container"})
-for ad in info_containers:
-    # print(ad)
-    # print(ad.find('a', {"class": "title"}).text.strip())
-    print(re.sub(' +', ' ', ad.find('a', {"class": "title"}).text.strip()))
-    print(ad.find('div', {"class": "price"}).text.strip())
-    if ad.find('div', {"class": "description"}) != "":
-        print(ad.find('div', {"class": "description"}).text.strip())
-    else:
-        print("No description.")
-    break
+def print_test():
+
+    ad_containers = soup.find_all('div', {'class': 'clearfix'})
+    # skip the first clearfix container
+    for ad_container in ad_containers:
+        # need to check if the title exists of the strip() will give errors
+        # could replace this with exception handling
+        if ad_container.find('div', {"class": "title"}):
+            print(ad_container.find('div', {"class": "title"}).text.strip())
+        if ad_container.find('div', {"class": "price"}):
+            print(re.sub(' +', ' ', ad_container.find('div', {"class": "price"}).text.strip()))
+        if ad_container.find('img'):
+            print(ad_container.find('img').get('data-src'))
+
+# print_test()
 
 
-images = soup.find_all('img')
-for image in images:
-    print(image)
-    print(image.get('data-src'))
+def first_page_scraper():
 
-    nametemp = image.get('alt')
+    titles = []
+    prices = []
+    image_links = []
 
-    i = 0
-    if len(nametemp) == 0:
-        filename = str(i)
-        i += 1
-    else:
-        filename = nametemp
+    ad_containers = soup.find_all('div', {'class': 'clearfix'})
 
-    image_link = image.get('data-src')
-    current_image = urllib.request.urlopen(image_link)
-    print(help(current_image))
-    print(current_image.length)
+    for ad_container in ad_containers:
+        # need to check if the title exists of the strip() will give errors
+        # could replace this with exception handling
+        if ad_container.find('div', {"class": "title"}) and ad_container.find('div', {"class": "price"}):
+            titles.append(ad_container.find('div', {"class": "title"}).text.strip())
+            prices.append(re.sub(' +', ' ', ad_container.find('div', {"class": "price"}).text.strip()))  # strip inner whitespace
+            image_links.append(ad_container.find('img').get('data-src'))
 
-    # change directory to save
-    os.chdir(r'C:\Users\Nicholas\Programming\ArtPriceEstimator\images')
 
-    if current_image.length > 1000:
-        imagefile = open(filename + ".jpeg", 'wb')
-        imagefile.write(urllib.request.urlopen(image_link).read())
-        imagefile.close()
 
-    break
+    return titles, prices, image_links
+
+
+titles, prices, image_links = first_page_scraper()
+print("done")
+
+
+def saving_pictures():
+
+
+    # ----- getting the images ----
+
+    images = soup.find_all('img')
+    for image in images:
+        print(image)
+        print(image.get('data-src'))
+
+    # -----saving the images to directory----
+
+        nametemp = image.get('alt')
+
+        i = 0
+        if len(nametemp) == 0:
+            filename = str(i)
+            i += 1
+        else:
+            filename = nametemp
+
+        image_link = image.get('data-src')
+        current_image = urllib.request.urlopen(image_link)
+        print(help(current_image))
+        print(current_image.length)
+
+        # change directory to save
+        os.chdir(r'C:\Users\Nicholas\Programming\ArtPriceEstimator\images')
+
+        if current_image.length > 1000:
+            imagefile = open(filename + ".jpeg", 'wb')
+            imagefile.write(urllib.request.urlopen(image_link).read())
+            imagefile.close()
+
+        break
+
 
 
 # a method for getting all the links on the page
